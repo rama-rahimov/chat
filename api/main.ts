2dc16a42588b97f05b68403e37aba75044bc4b68
Ramiz <109864769+rama-rahimov@users.noreply.github.com>
2025-11-14 18:10:44 +0400

    Update main.ts

--- api/main.ts
+++ api/main.ts
@@ -5,7 +5,7 @@ import {ValidationPipe} from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://chat-test-api-g5f4.onrender.com',
+    origin: 'https://chat-test-nuaq.onrender.com/',
     methods: 'GET,HEAD,PUT,POST,DELETE,OPTIONS',
     credentials: true,
   });
