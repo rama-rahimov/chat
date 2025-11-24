a30d40942386cab5ced4f84cef86f3635c44f1d4
Ramiz <109864769+rama-rahimov@users.noreply.github.com>
2025-11-24 16:20:54 +0400

    Update Llama.infrastructure.ts

--- api/src/Llama/infrastructure/Llama.infrastructure.ts
+++ api/src/Llama/infrastructure/Llama.infrastructure.ts
@@ -7,12 +7,14 @@ export class LlamaInfrastructure implements BotRepository{
     async answer(message:string):Promise<any>{
     const openChatAI = new OpenAI({
         baseURL: "https://openrouter.ai/api/v1",
-        apiKey: "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",
+        apiKey: "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",
         defaultHeaders: {
             "HTTP-Referer": "https://chat-test-nuaq.onrender.com/", // Optional. Site URL for rankings on openrouter.ai.
             "X-Title": "chat", // Optional. Site title for rankings on openrouter.ai.
         },
     });
+    const taakk = await openChatAI.models.list();
+    console.log({ taakk });
     const result =   await openChatAI.chat.completions.create({
       model: "meta-llama/llama-3.1-8b-instruct",
       messages: [
