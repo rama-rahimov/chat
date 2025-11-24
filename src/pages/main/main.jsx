6b3bcdc56c4204f00a1ddf4d8f72123f0be85adb
Ramiz <109864769+rama-rahimov@users.noreply.github.com>
2025-11-24 14:49:17 +0400

    Update main.jsx

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -14,14 +14,14 @@ function Main() {
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (!token) return ;
-        fetch(`/api/profile/me`, {
+        fetch(`${import.meta.env.VITE_TEST_API_URL}/api/profile/me`, {
             headers: {
                 Authorization: `Bearer ${token}`,
             }
         }).then((response) => response.json()).then((data) => {
             setMyData(data);
         })
-        const newSocket = io(`${import.meta.env.VITE_LOCAL_URL}/chat`, {
+        const newSocket = io(`${import.meta.env.VITE_TEST_API_URL}/chat`, {
             autoConnect:false,
             auth: { token }
         });
