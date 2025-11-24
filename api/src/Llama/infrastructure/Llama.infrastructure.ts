a6bc8250b864f5c8dbf904fea85595ad5f448881
Ramiz <109864769+rama-rahimov@users.noreply.github.com>
2025-11-24 15:48:36 +0400

    Update Llama.infrastructure.ts

--- api/src/Llama/infrastructure/Llama.infrastructure.ts
+++ api/src/Llama/infrastructure/Llama.infrastructure.ts
@@ -7,7 +7,7 @@ export class LlamaInfrastructure implements BotRepository{
     async answer(message:string):Promise<any>{
     const openChatAI = new OpenAI({
         baseURL: "https://openrouter.ai/api/v1",
-        apiKey: "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",
+        apiKey: "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",
         defaultHeaders: {
             "HTTP-Referer": "http://localhost:5173/", // Optional. Site URL for rankings on openrouter.ai.
             "X-Title": "chat", // Optional. Site title for rankings on openrouter.ai.
